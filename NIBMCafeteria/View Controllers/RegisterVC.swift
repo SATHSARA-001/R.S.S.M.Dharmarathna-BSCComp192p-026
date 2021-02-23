0a21e2f70672a6c308230f583a538bd833e5638a
Sathsara Maduranga <sathsara.emedia@gmail.com>
2021-02-23 21:20:19 +0530

    firebase certificate added

--- NIBMCafeteria/View Controllers/RegisterVC.swift
+++ NIBMCafeteria/View Controllers/RegisterVC.swift
@@ -6,24 +6,51 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterVC: UIViewController {
-
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func loginUser(){
+        // TODO: Validate Text Fields
+        
+        // Create cleaned versions of the text field
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // Signing in the user
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if error != nil {
+                print("Error")
+                //                // Couldn't sign in
+                //                self.errorLabel.text = error!.localizedDescription
+                //                self.errorLabel.alpha = 1
+            }
+            else {
+                
+                print("Logged In Succefully")
+                //                let homeViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                //
+                //                self.view.window?.rootViewController = homeViewController
+                //                self.view.window?.makeKeyAndVisible()
+            }
+        }
     }
-    */
-
+    
+    
+    @IBAction func login(_ sender: Any) {
+        loginUser()
+    }
+    
+    
+    
+    
 }
