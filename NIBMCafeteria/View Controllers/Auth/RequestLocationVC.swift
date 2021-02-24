d0eebaeb276ec18665e828366f32ccd8393aa4b6
Sathsara Maduranga <sathsara.emedia@gmail.com>
2021-02-24 21:39:04 +0530

    request location vc added

---
+++ NIBMCafeteria/View Controllers/Auth/RequestLocationVC.swift
@@ -0,0 +1,20 @@
+//
+//  RequestLocationVC.swift
+//  NIBMCafeteria
+//
+//  Created by Sathsara Maduranga on 2021-02-24.
+//
+
+import UIKit
+
+class RequestLocationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
